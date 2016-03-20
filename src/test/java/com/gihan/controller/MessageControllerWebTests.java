07a7d78ac96400e17df290677e2c170cac36475f
gihanf <gihan.fernando@gmail.com>
2016-03-20 22:27:05 +1100

    Fixed controller web test. Full build does not pass, controller test in isolation works.

--- src/test/java/com/gihan/controller/MessageControllerWebTests.java
+++ src/test/java/com/gihan/controller/MessageControllerWebTests.java
@@ -73,7 +73,7 @@ public class MessageControllerWebTests {
 
     @Test
     public void testCreate() throws Exception {
-        this.mockMvc.perform(post("/").param("text", "FOO text").param("summary", "FOO"))
+        this.mockMvc.perform(post("/").param("text", "FOO text").param("summary", "FOO").param("amount", "3.14"))
                 .andExpect(status().isMovedTemporarily())
                 .andExpect(header().string("location", RegexMatcher.matches("/[0-9]+")));
     }
